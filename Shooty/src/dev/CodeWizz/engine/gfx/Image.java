c8f1b55be642e86de9f8152a1afe31c94dd8e362
jCodeWizz <91249470+jCodeWizz@users.noreply.github.com>
2021-11-24 14:12:35 +0100

    E

--- Shooty/src/dev/CodeWizz/engine/gfx/Image.java
+++ Shooty/src/dev/CodeWizz/engine/gfx/Image.java
@@ -1,5 +1,7 @@
 package dev.CodeWizz.engine.gfx;
 
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -16,29 +18,54 @@ public class Image {
 	private int lightBlock = Light.NONE;
 	private Image normalMap;
 	private boolean hasNormalMap;
-	
+
 	public Image(String path) {
-		
+
 		try {
 			image = ImageIO.read(Image.class.getResourceAsStream(path));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		w = image.getWidth();
 		h = image.getHeight();
-		
+
 		p = image.getRGB(0, 0, w, h, null, 0, w);
 	}
-	
+
 	public Image(BufferedImage image) {
-		
+
 		w = image.getWidth();
 		h = image.getHeight();
-		
+
 		p = image.getRGB(0, 0, w, h, null, 0, w);
 	}
+
+	private static AffineTransform rotateImageClockWise(BufferedImage image, double angle) {
+
+		double rad = Math.toRadians(angle);
+		int imageWidth = image.getWidth();
+		int imageHeight = image.getHeight();
+
+		AffineTransform affineTransform = new AffineTransform();
+		affineTransform.rotate(rad, imageWidth / 2, imageHeight / 2);
+
+		double offset = (imageWidth - imageHeight) / 2;
+		affineTransform.translate(offset, offset);
+		return affineTransform;
+	}
 	
+	public static Image rotateImage(Image image, double angle) {
+		BufferedImage i = image.image;
+		BufferedImage output = new BufferedImage(i.getHeight(), i.getWidth(), i.getType());
+		
+		AffineTransform affineTransform = rotateImageClockWise(i, angle);
+        AffineTransformOp affineTransformOp = new AffineTransformOp(affineTransform, AffineTransformOp.TYPE_BILINEAR);
+        affineTransformOp.filter(i, output);
+        
+        return new Image(output);
+	}
+
 	public Image(int[] p, int w, int h) {
 		this.p = p;
 		this.w = w;
