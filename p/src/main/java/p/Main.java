8a5a0d35c9849303a003ba12aa4c23a3b41794d6
CodeWizz <91249470+jCodeWizz@users.noreply.github.com>
2022-09-01 09:05:47 +0200

    e

---
+++ p/src/main/java/p/Main.java
@@ -0,0 +1,21 @@
+package p;
+
+import java.io.File;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Tesseract tesseract = new Tesseract();
+		tesseract.setDatapath("C:\\Users\\tomhe\\Desktop\\OCR\\Tess4J\\tessdata");
+		tesseract.setLanguage("ENG");
+		try {
+			String text = tesseract.doOCR(new File("C:\\Users\\tomhe\\Desktop\\Java Coding\\Eclipse\\fabric-example-mod-1.18\\slimy\\p\\src\\main\\java\\p\\test4.JPG"));
+			System.out.println(text);
+		} catch (TesseractException e) {
+			e.printStackTrace();
+		}
+	}
+}
